69bb44315e1266507adf5ad285ecc5edb3362ad8
Raul Macarie <raul@macarie.me>
2025-11-11 23:51:26 +0100

    feat(ui): create reference/actual slider for `toMatchScreenshot` failures

---
+++ packages/ui/client/components/artifacts/ArtifactTemplate.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useSlots } from 'vue'
+
+const slots = useSlots()
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <header>
+      <h1>
+        <slot name="title" />
+      </h1>
+    </header>
+    <p v-if="slots.message">
+      <slot name="message" />
+    </p>
+    <slot />
+  </div>
+</template>
